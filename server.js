fac1234b0a042f59eb35a83fbab394303b067c71
symansu <36658480+symansu@users.noreply.github.com>
2018-03-20 20:59:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
 });
 app.get('/articleName',  function(req,  res){
     //articleName == article-one
-    //article[articleName] == {}content object for article one
+    //articles[articleName] == {}content object for article one
     var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
